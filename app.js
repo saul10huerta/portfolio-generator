a4d08adf2ff1c9c2c05986e121ac302c9529c9d6
Saul Everardo Huerta Perez <Saul@Sauls-MBP.attlocal.net>
2020-05-13 23:31:01 -0500

    captured used input with command-line arguments

---
+++ app.js
@@ -0,0 +1,24 @@
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+
+// Notice the lack of parentheses around the `profileDataArr` parameter?
+const printProfileData = profileDataArr => {
+    for (let i = 0; i < profileDataArr.length; i += 1) {
+      console.log(profileDataArr[i]);
+    }
+
+
+    console.log('================');
+
+    profileDataArr.forEach(function(profileItem) {
+    console.log(profileItem);
+    });
+
+
+    console.log('================');
+
+    profileDataArr.forEach((profileItem) => {console.log(profileItem)});
+
+};
+  
+printProfileData(profileDataArgs);
+
